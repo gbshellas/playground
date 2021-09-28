f8c5fb6f14346236ec804e513ab39a3b59a00d9b
WINCOR-NIXDORF\george.barbagiannis <george.barbagiannis@dieboldnixdorf.com>
2021-09-28 14:43:16 +0300

    initial add

--- python/tutorial4.py
+++ python/tutorial4.py
@@ -1,2 +1,12 @@
 #Lambda expressions tutorial
 
+def func1(x, y) :
+    return x(y)
+
+l = lambda a : a + 12
+print(func1(l, 19))
+
+print(func1(lambda a : a * 25, 2))
+
+x = lambda a : print(a)
+x(12)
---
+++ python/tutorial5.py
@@ -0,0 +1,12 @@
+# Lists tutorial
+
+l = list()
+
+l.append("ferrari")
+l.append("lamborgini")
+l.append("bughatti")
+
+print(l)
+
+l.sort()
+print(l)
