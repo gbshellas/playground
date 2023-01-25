03dd2a4f7e1880a0aaf30566061bec2ffa30993c
George Barbagiannis <gbshellas@yahoo.com>
2023-01-25 10:50:33 +0200

    Developer 2 wrote these changes

--- sql/select_statement.sql
+++ sql/select_statement.sql
@@ -1,2 +1,6 @@
 select * from table1;
+-- This is something else from another developer called dev2
 select * from table2;
+
+select the_baby_field, a_new_field from other_table2 
+	inner join table3 on (other_table2.id = table3.FK_id);
